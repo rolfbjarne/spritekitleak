0ac94dea3030c43a335c4c9687643db7b130782f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2014-04-01 16:43:40 +0200

    Initial Commit

---
+++ spritekitleak/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  spritekitleak
+//
+//  Created by Rolf Bjarne Kvinge on 01/04/14.
+//  Copyright (c) 2014 Rolf Bjarne Kvinge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ spritekitleak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  spritekitleak
+//
+//  Created by Rolf Bjarne Kvinge on 01/04/14.
+//  Copyright (c) 2014 Rolf Bjarne Kvinge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
