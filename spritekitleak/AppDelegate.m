3386f1caf182967ce1955f24294c6e17249ed0eb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2014-04-01 17:16:29 +0200

    Add test case.

--- spritekitleak/AppDelegate.m
+++ spritekitleak/AppDelegate.m
@@ -6,16 +6,86 @@
 //  Copyright (c) 2014 Rolf Bjarne Kvinge. All rights reserved.
 //
 
+#import <SpriteKit/SpriteKit.h>
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
+UIWindow *window = nil;
+UIViewController *vc = nil;
+UIViewController *vc2 = nil;
+UINavigationController *nvc = nil;
+UIButton *button = nil;
+BOOL showing = NO;
+
+void buttonClick ()
+{
+    
+    if (showing) {
+        [nvc popViewControllerAnimated:YES];
+    } else {
+//    var vc2 = new UIViewController ();
+    vc2 = [[UIViewController alloc] init];
+//    var skView = new SKView (window.Bounds);
+    SKView *skView = [[SKView alloc] initWithFrame:window.bounds];
+//    vc2.View.AddSubview (skView);
+    [vc2.view addSubview:skView];
+    
+    // Create and configure the scene.
+//    var scene = new SKScene (skView.Bounds.Size);
+    SKScene *scene = [[SKScene alloc] initWithSize:skView.bounds.size];
+//    scene.ScaleMode = SKSceneScaleMode.AspectFill;
+    scene.scaleMode  = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+//    skView.PresentScene(scene);
+    [skView presentScene:scene];
+    
+    //nvc.PushViewController (vc2, true);
+    [nvc pushViewController:vc2 animated:YES];
+//    GC.Collect ();
+    }
+    
+    showing = !showing;
+    
+    int64_t delayInSeconds = 2; // Your Game Interval as mentioned above by you
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        buttonClick ();
+    });
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    window.backgroundColor = [UIColor whiteColor];
+    
+    //vc = new UIViewController ();
+    vc = [[UIViewController alloc] init];
+//    button = new UIButton (window.Bounds);
+    button = [[UIButton alloc] initWithFrame: window.bounds];
+//    button.BackgroundColor = UIColor.Blue;
+    [button setBackgroundColor: [UIColor whiteColor]];
+//    button.SetTitle ("Go to SKScene", UIControlState.Normal);
+    [button setTitle:@"Go to SKScene" forState:UIControlStateNormal];
+    
+    
+    //vc.View.AddSubview (button);
+    [vc.view addSubview:button];
+    
+    nvc = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    window.rootViewController = nvc;
+    
+    [window makeKeyAndVisible];
+    
+    int64_t delayInSeconds = 2; // Your Game Interval as mentioned above by you
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        buttonClick ();
+    });
     return YES;
 }
 
